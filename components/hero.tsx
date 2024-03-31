0865dc8ec863419fcd8021b042c96327dafdb05f
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-03-31 17:32:33 -0400

    demo video

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,23 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import HeroImage from '@/public/images/dashboard.png'
-import Stats from '@/components/stats'
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import HeroImage from "@/public/images/dashboard.png";
+import Stats from "@/components/stats";
+import YouTube, { YouTubeProps } from "react-youtube";
+
+const opts: YouTubeProps["opts"] = {
+  height: "620",
+  width: "1104",
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+  },
+};
+
+const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+  // access to player in all event handlers via event.target
+  event.target.pauseVideo();
+};
 
 export default function Hero() {
   return (
@@ -10,39 +26,45 @@ export default function Hero() {
         {/* Section content */}
         <div className="px-4 sm:px-6">
           <div className="max-w-3xl mx-auto">
-              <div className="text-center pb-12 md:pb-16">
-                <h1 className="font-inter-tight text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-zinc-500 via-zinc-900 to-zinc-900 pb-4">
-                  <div>Meet Rachel: </div>
-                  The ultimate AI paralegal.
-                </h1>
-                <p className="text-lg text-zinc-500 mb-8">
-                  With unmatched sophistication and precision, 
-                  Rachel integrates user-provided 
-                  documents with extensive online research, 
-                  crafting comprehensive legal arguments and 
-                  briefs with unparalleled efficiency. Rachel 
-                  sets the standard for intelligent paralegal support for small to medium sized law firms.
-
-                </p>
-                <div className="max-w-xs mx-auto sm:max-w-none sm:inline-flex sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4">
-                  <div>
-                    <Link className="btn text-zinc-100 bg-zinc-900 hover:bg-zinc-800 w-full shadow" href="/request-demo">Request Demo</Link>
-                  </div>
-                  <div>
-                  </div>
+            <div className="text-center pb-12 md:pb-16">
+              <h1 className="font-inter-tight text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-zinc-500 via-zinc-900 to-zinc-900 pb-4">
+                <div>Meet Rachel: </div>
+                The ultimate AI paralegal.
+              </h1>
+              <p className="text-lg text-zinc-500 mb-8">
+                With unmatched sophistication and precision, Rachel integrates
+                user-provided documents with extensive online research, crafting
+                comprehensive legal arguments and briefs with unparalleled
+                efficiency. Rachel sets the standard for intelligent paralegal
+                support for small to medium sized law firms.
+              </p>
+              <div className="max-w-xs mx-auto sm:max-w-none sm:inline-flex sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4">
+                <div>
+                  <Link
+                    className="btn text-zinc-100 bg-zinc-900 hover:bg-zinc-800 w-full shadow"
+                    href="/request-demo"
+                  >
+                    Request Demo
+                  </Link>
                 </div>
+                <div></div>
               </div>
+            </div>
           </div>
         </div>
         {/* Image */}
         <div className="max-w-6xl mx-auto px-4 sm:px-6 flex justify-center pb-12 md:pb-20 relative before:absolute before:-top-12 before:w-96 before:h-96 before:bg-zinc-900 before:opacity-[.15] before:rounded-full before:blur-3xl before:-z-10">
-          <Image className="rounded-lg shadow-2xl" src={HeroImage} width={1104} height={620} alt="Hero" priority />
+          <YouTube
+            videoId="P_0tqqeDyTk"
+            opts={opts}
+            className="w-full rounded-lg"
+            onReady={onPlayerReady}
+          />
         </div>
 
         {/* Stats */}
         <Stats />
-
       </div>
     </section>
-  )
+  );
 }
